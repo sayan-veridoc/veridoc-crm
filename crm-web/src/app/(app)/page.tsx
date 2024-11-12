37a1a9eadf8b8781b816024c74cd634573c5969a
sayan-veridoc <dassayan004@gmail.com>
2024-11-12 20:30:24 +0530

    line fix

--- crm-web/src/app/(app)/page.tsx
+++ crm-web/src/app/(app)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <section className="items-center justify-items-center min-h-screen  ">
+    <section className="items-center justify-items-center min-h-screen">
       <p>Home</p>
     </section>
   );
